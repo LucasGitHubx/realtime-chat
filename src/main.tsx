752b561571e05de32a7e5e5270b4826063d0d6cb
LucasGitHubx <lucasdarosa450@gmail.com>
2024-08-28 14:14:50 -0300

    Rendering App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<h1>Hello</h1>);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
