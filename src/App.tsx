ecf959af95a21648570baa32e01bf557f0ceec51
LucasGitHubx <lucasdarosa450@gmail.com>
2024-09-01 19:19:01 -0300

    I added the login-page into de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import {
   Route,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import MainPage from "./pages/MainPage";
+import MainPage from "./pages/mainPage/MainPage";
+import LoginPage from "./pages/loginPage/LoginPage";
 
 export default function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/realtime-chat/" element={<MainLayout />}>
         <Route index element={<MainPage />} />
+        <Route path="/realtime-chat/login" element={<LoginPage />} />
       </Route>
     )
   );
